7f406ff5905f74ef4f118c8aa556f811cf383cd9
amosyeboah <amosfrimpong56@gmail.com>
2023-03-26 10:34:28 -0700

    0-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i); /* This cause an infinte loop */
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
