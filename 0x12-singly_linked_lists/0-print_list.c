bb1e155777a44eeb36ccf2ce34b001b56a867bd9
amosyeboah <amosfrimpong56@gmail.com>
2023-05-01 14:54:39 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,19 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-
-	while (h)
-	{
-		if (!h->str)
+	
+	while (h != NULL)
+	{	
+		if (h -> str! = NULL)
+		{
+			printf("%s\n", h -> str);
+		}
+		else{
 			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		count++;
+
+		}
+		h = h -> next;
+		count ++;
 	}
 
-	return (count);
 }
-
